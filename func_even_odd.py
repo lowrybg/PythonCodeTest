24699be5fa60f07af839d8ea9048960bd530aa85
lowry <lowrybg@users.noreply.github.com>
2026-01-28 19:08:35 +0200

    refactoring

--- func_even_odd.py
+++ func_even_odd.py
@@ -1,8 +1,6 @@
 def even_odd(*args):
     command = args[-1]
 
-
-
     if command == "even":
         return [x for x in args[:-1] if x % 2 == 0]
     elif command == "odd":
@@ -10,4 +8,4 @@ def even_odd(*args):
 
 print(even_odd(2,3,4,5,6,7,8,9, "even"))
 print(even_odd(2,3,4,5,6,7,8,9, "odd"))
-print(even_odd(2,3,4,5,6,7,8,9))
+print(even_odd(2,3,4,5,6,7,8,9, "even"))
