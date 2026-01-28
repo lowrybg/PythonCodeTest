54d6c2124da39657b02154a883d6426963bf6ad7
Taner <lowrybg@users.noreply.github.com>
2026-01-28 18:51:06 +0200

    removed .idea

---
+++ func_even_odd.py
@@ -0,0 +1,13 @@
+def even_odd(*args):
+    command = args[-1]
+
+
+
+    if command == "even":
+        return [x for x in args[:-1] if x % 2 == 0]
+    elif command == "odd":
+        return [x for x in args[:-1] if x % 2 != 0]
+
+print(even_odd(2,3,4,5,6,7,8,9, "even"))
+print(even_odd(2,3,4,5,6,7,8,9, "odd"))
+print(even_odd(2,3,4,5,6,7,8,9))
